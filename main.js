7ac7a345deaab9c2544db46b7d784569d61b83d3
smy-bs <bs.miyoung@gmail.com>
2023-04-07 10:32:35 +0200

    modification

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
 });
 
-// Projects
+// Projects 
 const workBtnContainer = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
